326f4e0d5e80ddc3c0aeeaf1b8e2e2b516ec0d2b
Matt Williams <matt@milliams.com>
2017-08-25 14:35:22 +0100

    Add tests for util

--- src/util.rs
+++ src/util.rs
@@ -13,3 +13,26 @@ pub fn transform_array_of_u8_to_u16(x: [u8; 2]) -> u16 {
 pub fn bound<T: Ord>(lower: T, upper: T, value: T) -> T {
     max(min(value, upper), lower)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_transform_u16_to_array_of_u8() {
+        assert_eq!(transform_u16_to_array_of_u8(0x3C81), [0x3C, 0x81]);
+    }
+
+    #[test]
+    fn test_transform_array_of_u8_to_u16() {
+        assert_eq!(transform_array_of_u8_to_u16([0x3C, 0x81]), 0x3C81);
+    }
+
+    #[test]
+    fn test_bound() {
+        assert_eq!(bound(-20, 500, 100), 100);
+        assert_eq!(bound(-20, 500, 600), 500);
+        assert_eq!(bound(-20, 500, -5), -5);
+        assert_eq!(bound(-20, 500, -400), -20);
+    }
+}
